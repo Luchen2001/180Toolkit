4bdc77273571960bf8891675a3051594d74779ab
Luchen2001 <luczhou@student.unimelb.edu.au>
2023-09-15 22:45:37 +1000

    change api url to http://127.0.0.1:3000

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", // Replace with your API's base URL
+  baseURL: "http://127.0.0.1:3000", // Replace with your API's base URL
   timeout: 10000, // Set your desired timeout for requests
   headers: {
     'Content-Type': 'application/json',
